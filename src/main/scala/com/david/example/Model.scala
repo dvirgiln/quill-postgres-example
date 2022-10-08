7901fc2662cd5c4a85ab79c5e2b09b17b10b4eb0
davidvirgilnaranjo <david.naranjo@copper.co>
2022-10-08 16:55:16 +0200

    Revert "trying to add json encoder decoders based on circe"
    This reverts commit 19d555565a8a0c7ea720ca7e978de863141cf5e2.

--- src/main/scala/com/david/example/Model.scala
+++ src/main/scala/com/david/example/Model.scala
@@ -3,11 +3,9 @@ package com.david.example
 import java.util.Date
 
 object Model {
-
-  import io.circe.generic.JsonCodec, io.circe.syntax._
   case class Film(id: Option[Long], title: String, did: Int, date_prod: Date, kind: String)
 
   case class Cinema(id: Option[Long], title: String, extra: CinemaExtra)
 
-  @JsonCodec case class CinemaExtra(city: String, address: String, phoneNumber: String, number_screens: Int)
+  case class CinemaExtra(city: String, address: String, phoneNumber: String, number_screens: Int)
 }
--- src/main/scala/com/david/example/ctx/CustomSqlContext.scala
+++ src/main/scala/com/david/example/ctx/CustomSqlContext.scala
@@ -1,16 +1,11 @@
 package com.david.example.ctx
 
-import java.sql.Types
-
-import com.david.example.Model.{Cinema, CinemaExtra, Film}
+import com.david.example.Model.{Cinema, Film}
 import com.github.jasync.sql.db.RowData
-import io.circe.Json
 import io.getquill.PostgresJAsyncContext
 import io.getquill.Literal
-class CustomSqlContext extends PostgresJAsyncContext(Literal, "testPostgresDB")
-  with CustomDecoders
-  with CustomEncoders
-{
+class CustomSqlContext extends PostgresJAsyncContext(Literal, "testPostgresDB"){
+
   implicit val filmsSchemaMeta = schemaMeta[Film]("films")
   implicit val cinemasSchemaMeta = schemaMeta[Cinema]("cinemas")
 
